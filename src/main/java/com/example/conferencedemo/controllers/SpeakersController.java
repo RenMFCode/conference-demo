78557c1819f2aee80a4fb57b0c29ffb4709f19c6
Renato Farinha <renatof@hadean.com>
2023-06-16 17:35:46 +0100

    add new controllers for sessions & speakers

---
+++ src/main/java/com/example/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,30 @@
+package com.example.conferencedemo.controllers;
+
+import com.example.conferencedemo.models.Session;
+import com.example.conferencedemo.models.Speaker;
+import com.example.conferencedemo.repositories.SessionRepository;
+import com.example.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getOne(id);
+    }
+
+}
